c470838a3458d76a81b635e8c5e355a4515e7066
SmileNattakit <nattakit.rattanakeha@gmail.com>
2024-05-14 15:46:30 +0700

    finish task

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 // Exercise 2.1 create a expression function for checking if a number is even
 function checkEven(number) {
   if (number % 2 == 0) {
-    console.log("เป็นเลขคู่");
+    console.log(number + " เป็นเลขคู่");
   } else {
-    console.log("เป็นเลขคี่");
+    console.log(number + " เป็นเลขคี่");
   }
 }
+checkEven(3);
 checkEven(2);
 
 //  Exercise 2.2 converts the function from Exercise 2.1 into an arrow function
 
 const checkEvenArrow = (number) => {
   if (number % 2 == 0) {
-    console.log("เป็นเลขคู่");
+    console.log(number + " เป็นเลขคู่");
   } else {
-    console.log("เป็นเลขคี่");
+    console.log(number + " เป็นเลขคี่");
   }
 };
+checkEvenArrow(3);
 checkEvenArrow(4);
 
+const checkEvenArrow2 = (number) =>
+  number % 2 == 0
+    ? console.log(number + " เป็นเลขคู่")
+    : console.log(number + " เป็นเลขคี่");
+checkEvenArrow2(5);
+checkEvenArrow2(2);
